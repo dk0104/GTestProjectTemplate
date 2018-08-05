9a7ac1f917d48113bb56949a080c59236898cfdc
Denis Keksel <denis.keksel@protonmail.com>
2018-08-05 23:06:41 +0200

    Param Test first attempt.

--- test/MatrixSquareFinderTest.cpp
+++ test/MatrixSquareFinderTest.cpp
@@ -7,6 +7,53 @@
 
 using namespace std;
 
-class MatrixSquareFinderTest : public ::testing::TestWithParam<>{
+class MatrixTable{
+public:
+    virtual MatrixTable(vector<vector<char>>& inputVector,int result);
+    virtual ~MatrixTable();
+    virtual vector<vector<char>>* GetTable();
+    virtual int GetExpectedResult();
+};
+
+class PrecompMatrixTable: public MatrixTable{
+public:
+    PrecompMatrixTable(vector<vector<char>>& input,int result);
+    vector<vector<char>> *GetTable() override {
+        return table;
+    }
+
+    int GetExpectedResult() override {
+        return MatrixTable::GetExpectedResult();
+    }
+
+protected:
+    vector<vector<char>> table;
 
 };
+
+typedef MatrixTable* CreateMatrixTable();
+
+
+class MatrixSquareFinderTest : public ::testing::TestWithParam<CreateMatrixTable*>
+{
+public:
+    virtual ~MatrixSquareFinderTest(){
+        delete  squareField;
+    }
+    virtual void SetUp(){
+        squareField = (*GetParam())();
+    }
+    virtual void TearDown(){
+        delete squareField;
+        squareField = NULL;
+    }
+protected:
+    MatrixTable* squareField;
+};
+
+TEST_P(MatrixSquareFinderTest,RetunFalseForNonMatchingSquares){
+    
+
+}
+
+INSTANTIATE_TEST_CASE_P(InvalidFieldsTest,MatrixSquareFinderTest,Values());
